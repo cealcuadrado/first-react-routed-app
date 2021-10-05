9f31802692fdba96041ddcb1537a49f1aa77092c
CeAlCuadrado <cealcuadrado@outlook.com>
2021-10-05 19:33:49 -0300

    Redefinido ruteo básico

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,20 +1,36 @@
 import React from 'react';
-import About from '../main/About';
-import Contact from '../main/Contact';
-import Home from '../main/Home';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 class Layout extends React.Component {
-    render() {
-        return (
-            <div>
-                <p>Aquí va la navegación</p>
-                <hr />
-                <Home />
-                <About />
-                <Contact />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <Router>
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/about">About</a>
+              </li>
+              <li>
+                <a href="/contact">Contact</a>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main>
+          <Route exact path="/home" render={() => <h2>Home</h2>} />
+          <Route exact path="/">
+            <h2>Home</h2>
+          </Route>
+          <Route exact path="/about" render={() => <h2>About</h2>} />
+          <Route exact path="/contact" render={() => <h2>Contact</h2>} />
+        </main>
+      </Router>
+    );
+  }
 }
 
 export default Layout;
